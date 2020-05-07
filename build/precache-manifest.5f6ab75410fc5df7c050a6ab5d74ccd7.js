90f871cbac7e096658b5250cefd5a8c98af223e0
Snippy <rateforx@gmail.com>
2020-05-07 07:40:12 +0200

    README

---
+++ build/precache-manifest.5f6ab75410fc5df7c050a6ab5d74ccd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea37966020293960a511",
+    "url": "/static/css/main.5b76c53d.chunk.css"
+  },
+  {
+    "revision": "ea37966020293960a511",
+    "url": "/static/js/main.7e689d45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "637dfcd77c8bdd864524",
+    "url": "/static/js/2.03f1445a.chunk.js"
+  },
+  {
+    "revision": "f16b83f43b173be21e29da0cfb8b8189",
+    "url": "/index.html"
+  }
+];
